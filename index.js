4b9787f77391e0eff599826993dfe4e08903fac0
salt-dev <salt-dev@salt-devs-MacBook-Air-8.local>
2022-10-21 11:42:13 +0200

    “switching”

--- index.js
+++ index.js
@@ -4,4 +4,14 @@ const read = fs.readFileSync('./personData.json');
 
 const parsedData = JSON.parse(read.toString());
 
-console.log(parsedData.forEach( element => element.address ));
+// parsedData.forEach( element => console.log(element.address));
+
+// const over30 = parsedData.filter(item => item.address.city === 'Berkhout');
+// console.log(over30);
+
+const cityList = parsedData.reduce((acc, curr)=>{
+ acc[curr.address.city] = acc[curr.address.city] !== undefined ? acc[curr.address.city] + 1 : 1 ;
+ return acc;
+},{})
+
+console.log(cityList);
